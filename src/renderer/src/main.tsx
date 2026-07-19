22a7a4cf9a47d11fe180c8b2fb02ac2d58f975f1
gnehs <gnehs@gnehs.net>
2026-07-19 19:25:47 +0800

    feat: render app through TanStack Router

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -6,17 +6,18 @@ import '@fontsource/inter/latin-800.css'
 import '@fontsource/inter/latin-900.css'
 import './assets/main.css'
 
+import { RouterProvider } from '@tanstack/react-router'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App'
 import AppUpdateNotifier from './components/AppUpdateNotifier'
 import { Toaster } from './components/ui/sonner'
 import { TooltipProvider } from './components/ui/tooltip'
+import { router } from './router'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <TooltipProvider>
-      <App />
+      <RouterProvider router={router} />
       <AppUpdateNotifier />
       <Toaster position="bottom-right" />
     </TooltipProvider>
